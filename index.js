2164ab101004f9d8999f6c4bb6a4f70a995c8f3d
Joshua Kimbareeba <jkimbareeba@yahoo.com>
2019-10-16 11:07:09 +0300

    changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({"name":"this an OAuth test and feedback"});
+    res.send({'name': 'JOSHUA KIMBAREEBA'});
 });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
