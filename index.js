ac95205badbfee517ae56dbe51f534855e7d4fff
Joshua Kimbareeba <jkimbareeba@yahoo.com>
2019-10-17 11:39:24 +0300

    name-update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res, next) => {
-    res.send({'name': 'Brian'});
+    res.send({'name': 'Joshua kimbareeba'});
     next();
 });
 app.get('/dashboard', (req, res) =>{
