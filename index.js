a4cfa8a9c2d2384cff83ba99820e6c943bb8970c
Joshua Kimbareeba <jkimbareeba@yahoo.com>
2019-10-18 11:49:34 +0300

    updates-02

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res, next) => {
-    res.send({'name': 'Joshua kimbareeba'});
-    next();
-});
-app.get('/dashboard', (req, res) =>{
-    res.send({'description': 'A humbled Man'});
-})
+
+passport.use(new googleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done ) => {
+    console.log('accessToken',accessToken);
+    console.log('refresh token', refreshToken);
+    console.log('profile', profile);
+})); 
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);  
+ 
